6b8e74c065fc86fcf6f54355210286ae06c109c1
wupeiwen <javapeiwen2010@gmail.com>
2019-03-25 10:12:31 +0800

    项目初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+ <div id="home"></div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {}
+  },
+  created () {
+    console.log('osm-url', process.env.VUE_APP_OSMURL)
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
